9b671d7a44b1fb1fa58c103b871fe8dfed42d1f2
Richard Lum <a00470425@gmail.com>
2012-09-05 22:58:58 -0700

    Event handlers wired to add button and text box

--- StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockWatcher.java
@@ -1,6 +1,14 @@
 package com.google.gwt.sample.stockwatcher.client;
 
+import java.util.ArrayList;
+
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyPressEvent;
+import com.google.gwt.event.dom.client.KeyPressHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HorizontalPanel;
@@ -17,6 +25,7 @@ public class StockWatcher implements EntryPoint{
 	private TextBox newSymbolTextBox = new TextBox();
 	private Button addStockButton = new Button("Add");
 	private Label lastUpsdatedLabel = new Label();
+	private ArrayList<String> stocks = new ArrayList<String>();
 	
 	/**
 	 * Entry point method
@@ -43,5 +52,70 @@ public class StockWatcher implements EntryPoint{
 		
 		//Move cursor focus to input box
 		newSymbolTextBox.setFocus(true);
+		
+		//Listen for mouse events on add button
+		addStockButton.addClickHandler(new ClickHandler(){
+			public void onClick(ClickEvent event){
+				addStock();
+			}
+		});
+		
+		//Listen for keyboard
+		newSymbolTextBox.addKeyPressHandler(new KeyPressHandler(){
+
+			@Override
+			public void onKeyPress(KeyPressEvent event) {
+				if(event.getCharCode() == KeyCodes.KEY_ENTER){
+					addStock();
+				}
+				
+			}
+			
+		});
+		
 	}
+
+	protected void addStock() {
+		// TODO Auto-generated method stub
+		final String symbol = newSymbolTextBox.getText().toUpperCase().trim();
+		newSymbolTextBox.setFocus(true);
+		
+		if (!symbol.matches("^[0-9A-Za-z\\.]{1,10}$")){
+			Window.alert("'" + symbol + "' is not a valid symbol");
+			newSymbolTextBox.selectAll();
+			return;
+		}
+		
+		newSymbolTextBox.setText("");
+		
+		// dont add stock if its already in table
+		if (stocks.contains(symbol))
+			return;
+		
+		// add stock
+		int row = stocksFlexTable.getRowCount();
+		stocks.add(symbol);
+		stocksFlexTable.setText(row, 0, symbol);
+		
+		//add button to remove stock
+		Button removeStockButton = new Button("x");
+		removeStockButton.addClickHandler(new ClickHandler(){
+
+			@Override
+			public void onClick(ClickEvent event) {
+				int removedIndex = stocks.indexOf(symbol);
+				stocks.remove(removedIndex);
+				stocksFlexTable.removeRow(removedIndex+1);
+				
+			}
+			
+		});
+		stocksFlexTable.setWidget(row, 3, removeStockButton);
+		
+		// get stock price
+		
+	}
+	
+	
+	
 }
