1f28cac3986a82b766dcd52ecce23e76ad78c13b
Richard Lum <a00470425@gmail.com>
2012-09-06 15:13:25 -0700

    add StockPrice.java and test folder

---
+++ StockWatcher/src/com/google/gwt/sample/stockwatcher/client/StockPrice.java
@@ -0,0 +1,33 @@
+package com.google.gwt.sample.stockwatcher.client;
+
+public class StockPrice {
+	private String symbol;
+	private double price;
+	private double change;
+	
+	public StockPrice(String string, double price2, double change2) {
+		this.symbol = string;
+		this.price = price2;
+		this.change = change2;
+	}
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public double getChange() {
+		return change;
+	}
+	public void setChange(double change) {
+		this.change = change;
+	}
+	
+	
+}
